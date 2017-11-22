0b3d500418d7be374ac93c6a9a1aae2f7d8bf47c
ferney76 <ferney76@gmail.com>
2017-11-21 19:48:18 -0500

    master

---
+++ SumaGit/SumaGit/Calculos.cs
@@ -0,0 +1,46 @@
+﻿|using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SumaGit
+{
+    public class Calculos
+    {
+        //crear atributos (Campos)
+        public short nUno, nDos;
+
+        // creamos el metodo sumar
+        short sumarNumeros = 0;
+
+        public void sumar(short numeroUno,short numeroDos)
+        {
+            sumarNumeros =(short) (numeroUno + numeroDos);
+        }
+
+        public void mostrarResultados()
+        {
+            Console.WriteLine("Total: " + sumarNumeros);
+               
+        }
+
+        public void metodouno()
+        {
+            Console.WriteLine("Total: " + sumarNumeros);
+            
+        }
+
+        public void metododos()
+        {
+            Console.WriteLine("Total: " + sumarNumeros);
+
+        }
+
+        public void metodotres()
+        {
+            Console.WriteLine("Total: " + sumarNumeros);
+
+        }
+    }
+}
---
+++ SumaGit/SumaGit/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SumaGit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hello Mundo");
+
+            Console.WriteLine("Numero Uno");
+            //con esta linae se lleva el dato
+            short numUno = short.Parse(Console.ReadLine());
+
+            Console.WriteLine("NUmero Dos");
+            short numDos = short.Parse(Console.ReadLine());
+
+            //instanciar la clase
+            Calculos realizarOperacion = new Calculos();
+            realizarOperacion.nUno = numUno;
+           // realizarOperacion.nDos = numDos;
+
+            Console.WriteLine(realizarOperacion.nUno + " " + realizarOperacion.nDos);
+            realizarOperacion.sumar(realizarOperacion.nUno, realizarOperacion.nDos);
+            realizarOperacion.mostrarResultados();
+            Console.ReadLine();
+        }
+    }
+}
